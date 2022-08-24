69a9f13cdc6823631ddc5e8412d511b92ccf9d85
Dan Kollar <danielkollar@outlook.com>
2022-08-24 08:20:13 -0400

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import { Color } from "three";
+import { IfcViewerAPI } from "web-ifc-viewer";
+
+const container = document.getElementById("viewer-container");
+const viewer = new IfcViewerAPI({
+  container,
+  backgroundColor: new Color(0xffffff),
+});
+viewer.axes.setAxes();
+viewer.grid.setGrid();
+
+const input = document.getElementById("file-input");
+
+window.ondblclick = () => viewer.IFC.selector.pickIfcItem(true);
+window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
+viewer.clipper.active = true;
+
+window.onkeydown = (event) => {
+  if (event.code === "KeyP") {
+    viewer.clipper.createPlane();
+  } else if (event.code === "KeyO") {
+    viewer.clipper.deletePlane();
+  }
+};
+
+input.addEventListener(
+  "change",
+
+  async (changed) => {
+    const file = changed.target.files[0];
+    const ifcURL = URL.createObjectURL(file);
+    viewer.IFC.loadIfcUrl(ifcURL);
+  },
+
+  false
+);
+
+async function loadIfc(url) {
+  const model = await viewer.IFC.loadIfcUrl(url);
+  viewer.shadowDropper.renderShadow(model.modelID);
+}
+
+// load model from the below path in your repository
+loadIfc("models/01.ifc");
